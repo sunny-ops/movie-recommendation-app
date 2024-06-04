a7576a154f80667d57a8e60a6551633105772359
shizhong <shizhong1989@gmail.com>
2024-06-04 11:51:09 -0700

    Build backend

---
+++ movies-backend/src/test/java/net/javaguides/movies/MoviesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

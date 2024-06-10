0f9b88060ab7681e9a045604db4f810de6b07fe7
shizhong <shizhong1989@gmail.com>
2024-06-10 14:59:54 -0700

    recommendation for movies using cast, keywords, director, genres as factors

--- content-based/moreFactors.py
+++ content-based/moreFactors.py
@@ -5,6 +5,10 @@ from sklearn.metrics.pairwise import linear_kernel
 import numpy as np
 # Parse the stringified features into their corresponding python objects
 from ast import literal_eval
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
 
 
 # Sample data
@@ -30,7 +34,6 @@ metadata = metadata.merge(credits, on='id')
 metadata = metadata.merge(keywords, on='id')
 
 print(metadata.head(2))
-
 
 
 features = ['cast', 'crew', 'keywords', 'genres']
@@ -89,47 +92,60 @@ def create_soup(x):
 metadata['soup'] = metadata.apply(create_soup, axis=1)
 
 print(metadata[['soup']].head(2))
-# # 1. Initialize TfidfVectorizer
-# tfidf = TfidfVectorizer(stop_words='english')
+# 1. Initialize TfidfVectorizer
+tfidf = TfidfVectorizer(stop_words='english')
+count = CountVectorizer(stop_words='english')
 
-# # Replace NaN with an empty string
-# metadata['overview'] = metadata['overview'].fillna('')
+# Replace NaN with an empty string
+metadata['soup'] = metadata['soup'].fillna('')
 
-# # Fit and transform the data
-# tfidf_matrix = tfidf.fit_transform(metadata['overview'])
+# Fit and transform the data
+tfidf_matrix = tfidf.fit_transform(metadata['soup'])
+count_matrix = count.fit_transform(metadata['soup'])
 
-# print(tfidf_matrix.shape)
-# # 2. Compute the cosine similarity matrix
-# cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-# print(cosine_sim.shape)
-# print(cosine_sim[1])
+print(tfidf_matrix.shape)
+# 2. Compute the cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
+print(cosine_sim.shape)
+print(cosine_sim[1])
 
-# # 3. recommend 10 similar movies
-# #Construct a reverse map of indices and movie titles
-# indices = pd.Series(metadata.index, index=metadata['title']).drop_duplicates()
-# print(indices[:10])
+# 3. recommend 10 similar movies
+#Construct a reverse map of indices and movie titles
+metadata = metadata.reset_index()
+indices = pd.Series(metadata.index, index=metadata['title']).drop_duplicates()
+print(indices[:10])
 
-# # Function that takes in movie title as input and outputs most similar movies
-# def get_recommendations(title, cosine_sim=cosine_sim):
-#     # Get the index of the movie that matches the title
-#     idx = indices[title]
+# Function that takes in movie title as input and outputs most similar movies
+def get_recommendations(title, cosine_sim=cosine_sim):
+    # Get the index of the movie that matches the title
+    idx = indices[title]
 
-#     # Get the pairwsie similarity scores of all movies with that movie
-#     sim_scores = list(enumerate(cosine_sim[idx]))
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
 
-#     # Sort the movies based on the similarity scores
-#     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-#     # Get the scores of the 10 most similar movies
-#     sim_scores = sim_scores[1:11]
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
 
-#     # Get the movie indices
-#     movie_indices = [i[0] for i in sim_scores]
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
 
-#     # Return the top 10 most similar movies
-#     return metadata['title'].iloc[movie_indices]
+    # Return the top 10 most similar movies
+    return metadata['title'].iloc[movie_indices]
 
-# print(get_recommendations('The Dark Knight Rises'))
+print(get_recommendations('The Dark Knight Rises', cosine_sim2))
 
+# recommendations = []
+
+# for title in metadata['title']:
+#     recommended_titles = get_recommendations(title)
+#     recommendations.append({'title': title, 'recommendations': recommended_titles})
+
+# recommendations_df = pd.DataFrame(recommendations)
+# print(recommendations_df.head(3))
+# recommendations_df.to_csv('movie_recommendations.csv', index=False)
 
 

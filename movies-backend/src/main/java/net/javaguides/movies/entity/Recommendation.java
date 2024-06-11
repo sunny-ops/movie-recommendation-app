c170b346f22632c18630684dd1186b546bddd776
shizhong <shizhong1989@gmail.com>
2024-06-11 10:16:47 -0700

    Add Recommendation entity

--- movies-backend/src/main/java/net/javaguides/movies/entity/Recommendation.java
+++ movies-backend/src/main/java/net/javaguides/movies/entity/Recommendation.java
@@ -1,2 +1,24 @@
-package net.javaguides.movies.entity;public class Recommendation {
+package net.javaguides.movies.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="recommendations")
+public class Recommendation {
+    @Id // primary key
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    @Column(name="Title")
+    private String title;
+    @Column(name="Recommends")
+    private String recommends;
 }
